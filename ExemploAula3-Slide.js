3687aa476d18d3d0fd9c8b2b883330b459c7c9f8
Douglas Américo <douglas@likesistemas.com.br>
2025-03-25 21:47:27 -0300

    exemplos do slide aula 3

---
+++ ExemploAula3-Slide.js
@@ -0,0 +1,7 @@
+console.log("A soma de 3 e 9 é: " + (3+9));
+console.log("A subtração de 9 por 3 é: "+(9-3));
+console.log("A divisão de 9 por 3 é: "+(9/3));
+console.log("A multiplicação de 3 por 9 é: "+(3*9));
+console.log("O resto da divisão de 9 por 3 é: "+(9%3));
+console.log("2 elevado a 3° potencia é: "+Math.pow(2,3));
+console.log("A raiz quadrada de 9 é: "+Math.sqrt(9));
