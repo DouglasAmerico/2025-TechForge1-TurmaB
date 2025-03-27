62f5ce6162d6a7589cc31bc86859657088297db8
Douglas Américo <douglas@likesistemas.com.br>
2025-03-27 19:23:13 -0300

    subdividindo as aulas em pastas

---
+++ Aula2/ExerciciosSlideAula2.js
@@ -0,0 +1,15 @@
+const leitura = require("prompt-sync")();
+
+let nome = leitura("Informe seu nome: ");
+console.log("O nome informado foi "+nome);
+let corFavorita = leitura("Informe sua cor preferida: ");
+console.log("A cor informada foi "+corFavorita);
+let filme = leitura("Informe o seu filme favorito: ");
+console.log("O filme informado foi "+filme);
+let cidade = leitura("Informe a sua cidade: ");
+console.log("A cidade informada foi "+cidade);
+let animalEstimacao = leitura("Informe o nome de seu animal: ");
+console.log("O nome do animal informado foi "+animalEstimacao);
+
+
+
